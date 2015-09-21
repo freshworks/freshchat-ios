c3e0991b8325f841a91befdd0ed43dd61e0e8771
kirthikakvs <kirthika@freshdesk.com>
2015-09-21 20:33:33 +0530

    Add Article detail controller .
    Signed-off-by: kirthikakvs <kirthika@freshdesk.com>

---
+++ SDK/HotlineSDK/HotlineSDK/Controllers/HLArticleDetailViewController.h
@@ -0,0 +1,20 @@
+//
+//  HLArticleDetailViewController.h
+//  HotlineSDK
+//
+//  Created by kirthikas on 21/09/15.
+//  Copyright © 2015 Freshdesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import "FDYesNoPromptView.h"
+
+//Implement FDYesNoPromptViewDelegate if needed
+@interface HLArticleDetailViewController : UIViewController <UIWebViewDelegate, UIScrollViewDelegate>
+
+@property (strong, nonatomic) NSString *articleDescription;
+@property (strong, nonatomic) NSNumber *articleID;
+
+- (instancetype)initWithModalPresentationType:(BOOL)isModalPresentation;
+
+@end
---
+++ SDK/HotlineSDK/HotlineSDK/Controllers/HLArticleDetailViewController.m
@@ -0,0 +1,298 @@
+//
+//  HLArticleDetailViewController.m
+//  HotlineSDK
+//
+//  Created by kirthikas on 21/09/15.
+//  Copyright © 2015 Freshdesk. All rights reserved.
+//
+
+//#import "FDReachability.h"
+//#import "FDFooterView.h"
+#import "HLArticleDetailViewController.h"
+//#import "FDVotingManager.h"
+//#import "FDSecureStore.h"
+//#import "FDTheme.h"
+//#import "FDBarButtonItem.h"
+//#import "FDMacros.h"
+//#import "FDAPIClient.h"
+//#import "FDCoreDataImporter.h"
+//#import "FDcoreDataCoordinator.h"
+//#import "FDConstants.h"
+//#import "FDArticle.h"
+#import <AVFoundation/AVFoundation.h>
+#define HL_THEMES_DIR @"Themes"
+
+@interface HLArticleDetailViewController ()
+
+
+@property (strong, nonatomic) UIWebView *webView;
+//@property (strong, nonatomic) FDTheme *theme;
+//@property (strong, nonatomic) FDReachability *reachability;
+//@property (strong, nonatomic) FDFooterView *footerView;
+//@property (strong, nonatomic) FDSecureStore *secureStore;
+//@property (strong, nonatomic) FDVotingManager *votingManager;
+@property (strong, nonatomic) NSLayoutConstraint         *promptViewHeightConstraint;
+@property (strong, nonatomic) UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) NSString *appAudioCategory;
+@property (nonatomic) BOOL isModalView;
+
+@end
+
+@implementation HLArticleDetailViewController
+
+@synthesize promptViewHeightConstraint;
+
+
+#pragma mark - Lazy Instantiations
+-(instancetype)initWithModalPresentationType:(BOOL)isModalPresentation {
+    self = [super init];
+    if (self) {
+        self.isModalView = isModalPresentation;
+    }
+    return self;
+}
+
+
+//-(FDTheme *)theme{
+//    if(!_theme){
+//        _theme = [FDTheme sharedInstance];
+//    }
+//    return _theme;
+//}
+//
+//-(FDSecureStore *)secureStore{
+//    if(!_secureStore){
+//        _secureStore = [FDSecureStore sharedInstance];
+//    }
+//    return _secureStore;
+//}
+//
+//-(FDVotingManager *)votingManager{
+//    if (!_votingManager) {
+//        _votingManager = [FDVotingManager sharedInstance];
+//    }
+//    return _votingManager;
+//}
+
+-(NSString *)embedHTML{
+    NSString *article = [self.articleDescription stringByReplacingOccurrencesOfString:@"src=\"//" withString:@"src=\"http://"];
+    article = [article stringByReplacingOccurrencesOfString:@"value=\"//" withString:@"value=\"http://"];
+    return [NSString stringWithFormat:@""
+            "<html>"
+            "<style type=\"text/css\">"
+            "%@" // CSS Content
+            "</style>"
+            "<body>"
+            "%@" // Article Content
+            "</body>"
+            "</html>", [self normalizeCssContent],article];
+}
+
+-(NSString *)normalizeCssContent{
+    NSBundle *hlResourceBundle = [self getHLResourceBundle];
+    NSString  *cssFilePath = [hlResourceBundle pathForResource:@"normalize" ofType:@"css" inDirectory:HL_THEMES_DIR];
+    NSData *cssContent = [NSData dataWithContentsOfFile:cssFilePath];
+    return [[NSString alloc]initWithData:cssContent encoding:NSUTF8StringEncoding];
+}
+
+-(NSBundle *)getHLResourceBundle{
+    NSBundle *HLResourceBundle = [NSBundle bundleWithURL:[[NSBundle mainBundle] URLForResource:@"MHResources" withExtension:@"bundle"]];
+    return HLResourceBundle;
+}
+
+#pragma mark - Life cycle methods
+
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    [self registerAppAudioCategory];
+//    [self theming];
+//    [self setNavigationItem];
+    [self setSubviews];
+//    [self checkNetworkReachability];
+//    [self fixAudioPlayback];
+    //    [self handleArticleVoteAfterSometime];
+}
+
+//-(void)handleArticleVoteAfterSometime{
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+//        [self handleArticleVotePrompt];
+//    });
+//}
+
+-(void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+//    [self resetAudioPlayback];
+}
+
+//-(void)theming{
+//    self.view.backgroundColor = [self.theme backgroundColorSDK];
+//}
+//
+//-(void)setNavigationItem{
+//    [self.navigationItem setTitle:FDLocalizedString(@"Solutions Detail Nav Bar Title Text" )];
+//    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+//    UIBarButtonItem * barButton = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicator];
+//    [self.navigationItem setRightBarButtonItem:barButton animated:YES];
+//    if(self.isModalView)
+//    {
+//        FDBarButtonItem *backButton = [[FDBarButtonItem alloc]initWithTitle:FDLocalizedString(@"Solutions Detail Nav Bar Back Button Text") style:UIBarButtonItemStylePlain target:self action:@selector(closeButton:)];
+//        [self.navigationItem setLeftBarButtonItem:backButton];
+//    }
+//}
+//
+//-(void)closeButton:(id)sender{
+//    [self dismissViewControllerAnimated:YES completion:nil];
+//}
+
+
+-(void)setSubviews{
+    //Webview
+    self.webView = [[UIWebView alloc]init];
+    self.webView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.webView.delegate = self;
+    self.webView.dataDetectorTypes = UIDataDetectorTypeAll;
+    self.webView.scrollView.delegate = self;
+    [self.webView loadHTMLString:self.embedHTML baseURL:nil];
+    [self.view addSubview:self.webView];
+    [self.webView setBackgroundColor:[UIColor whiteColor]];
+    
+//    FDYesNoPromptView *articleVotePrompt = [[FDYesNoPromptView alloc] initWithDelegate:self andKey:@"Article Vote Prompt"];
+//    articleVotePrompt.delegate = self;
+//    articleVotePrompt.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self.view addSubview:articleVotePrompt];
+//    
+//    self.footerView = [[FDFooterView alloc]initWithController:self];
+//    [self.view addSubview:self.footerView];
+//    
+//    
+//    self.promptViewHeightConstraint = [NSLayoutConstraint constraintWithItem:articleVotePrompt
+//                                                                   attribute:NSLayoutAttributeHeight
+//                                                                   relatedBy:NSLayoutRelationEqual
+//                                                                      toItem:nil
+//                                                                   attribute:NSLayoutAttributeNotAnAttribute
+//                                                                  multiplier:1.0
+//                                                                    constant:0];
+//    
+//    [self.view addConstraint:self.promptViewHeightConstraint];
+    
+    NSDictionary *views = @{@"webView" : self.webView};
+//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[promptView]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[webView]|" options:0 metrics:nil views:views]];
+//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[footerView]|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[webView]|" options:0 metrics:nil views:views]];
+    
+}
+
+//-(void)checkNetworkReachability{
+//    self.reachability = [FDReachability reachabilityWithHostname:@"www.google.com"];
+//    __weak typeof(self)weakSelf = self;
+//    //Internet is reachable
+//    self.reachability.reachableBlock = ^(FDReachability*reach){
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            [weakSelf.webView loadHTMLString:weakSelf.embedHTML baseURL:nil];
+//        });
+//    };
+//    [self.reachability startNotifier];
+//}
+
+#pragma mark - Webview delegate
+
+-(BOOL)webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
+    if (inType == UIWebViewNavigationTypeLinkClicked){
+        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        return NO;
+    }
+    return YES;
+}
+
+-(void)webViewDidStartLoad:(UIWebView *)webView{
+    [self.activityIndicator startAnimating];
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    [self.activityIndicator stopAnimating];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self.activityIndicator stopAnimating];
+}
+
+//Hack for playing audio on WebView
+-(void)registerAppAudioCategory{
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    self.appAudioCategory = audioSession.category;
+}
+
+//-(void) fixAudioPlayback {
+//    if([ self needAudioFix] ) {
+//        [self setAudioCategory:AVAudioSessionCategoryPlayback];
+//    }
+//}
+//
+//-(BOOL) needAudioFix {
+//    return (self.appAudioCategory &&
+//            ( self.appAudioCategory != AVAudioSessionCategoryPlayAndRecord
+//             && self.appAudioCategory != AVAudioSessionCategoryPlayback ) );
+//}
+//
+//-(void)resetAudioPlayback{
+//    if([self needAudioFix]) {
+//        [self setAudioCategory:self.appAudioCategory];
+//    }
+//}
+
+//-(void)setAudioCategory:(NSString *) audioSessionCategory{
+//    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+//    NSError *setCategoryError = nil;
+//    
+//    if (![audioSession setCategory:audioSessionCategory error:&setCategoryError]) {
+//        FDLog(@"%s setCategoryError=%@", __PRETTY_FUNCTION__, setCategoryError);
+//    }
+//    
+//}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    //    [self handleArticleVotePrompt];
+}
+
+//-(void)handleArticleVotePrompt{
+//    if (self.webView.scrollView.contentOffset.y >= (self.webView.scrollView.contentSize.height - self.webView.scrollView.frame.size.height)) {
+//        BOOL isArticleVoted = [self.votingManager isArticleVoted:self.articleID];
+//        if (!isArticleVoted) {
+//            [self showArticleRatingPrompt];
+//        }
+//    }
+//    else if(self.webView.scrollView.contentOffset.y >= 0 && self.webView.scrollView.contentOffset.y < (self.webView.scrollView.contentSize.height - self.webView.scrollView.frame.size.height)){
+//        [self hideArticleRatingPrompt];
+//    }
+//}
+//
+//-(void) showArticleRatingPrompt{
+//    [UIView animateWithDuration:.5 animations:^{
+//        self.promptViewHeightConstraint.constant = PROMPT_VIEW_HEIGHT;
+//        [self.view layoutIfNeeded];
+//    }];
+//}
+//
+//-(void) hideArticleRatingPrompt{
+//    [UIView animateWithDuration:.5 animations:^{
+//        self.promptViewHeightConstraint.constant = 0;
+//        [self.view layoutIfNeeded];
+//    }];
+//}
+//
+//-(void)yesButtonClicked:(id)sender{
+//    [self hideArticleRatingPrompt];
+//    [self.votingManager upVoteForArticle:self.articleID withCompletion:^(NSError *error) {
+//        FDLog(@"Voting Completed");
+//    }];
+//}
+//
+//-(void)noButtonClicked:(id)sender{
+//    [self hideArticleRatingPrompt];
+//    [self.votingManager downVoteForArticle:self.articleID withCompletion:^(NSError *error) {
+//        FDLog(@"Voting Completed");
+//    }];
+//}
+
+@end
