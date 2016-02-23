783403ae7b7cbe4f5737789b40ee9104b07a6a47
Hrishikesh Premkumar <hrishikesh@freshdesk.com>
2016-02-23 23:28:16 +0530

    Updated Comments for Documentation
    @vignesh-giri  @srikrishnan-ganesan  Incorporated your changes.
    
    Added return values as well ..

--- HotlineSDK/HotlineSDK/Hotline.h
+++ HotlineSDK/HotlineSDK/Hotline.h
@@ -14,35 +14,38 @@
 @interface HotlineConfig : NSObject
 
 /*
- * App ID of your App. This is used to authenticate the SDK for your app. Please check with your Admin SDK Integration settings.
+ * App ID of your App. This is used to identify the SDK for your app to hotline.io. 
+ * Please see API & App under Settings ( http://web.hotline.io/settings/apisdk ) to get your App ID.
  */
 @property (strong, nonatomic) NSString *appID;
 /*
- * App Key of your App. This is used to authenticate the SDK for your app. Please check with your Admin SDK Integration settings.
+ * App Key of your App. This is used to authenticate the SDK for your app to hotline.io.
+ * Please see API & App under Settings ( http://web.hotline.io/settings/apisdk ) to get your App Key.
  */
 @property (strong, nonatomic) NSString *appKey;
 /*
- * Domain for Hotline. This defaults to app.hotline.io. If you were using Konotor SDK earlier please set it to app.konotor.com.
+ * Domain for Hotline. Do not change this. Set to "https://app.konotor.com" if you are an old customer of Konotor
  */
 @property (strong, nonatomic) NSString *domain;
 /*
- * Enable/disable voice messages. When enabled user can record and send audio attachments on the chat. Default NO.
+ * Enable/disable voice messages. When enabled, users can record and send audio messages to the Agents. Default is set to NO.
  */
 @property (nonatomic, assign) BOOL voiceMessagingEnabled;
 /*
- * Enable/disable picture messages. When enabled users can send picture attachments on the chat. Default YES.
+ * Enable/disable picture messages. When enabled, users can send images over chat. Default is set to YES.
  */
 @property (nonatomic, assign) BOOL pictureMessagingEnabled;
 /*
- * Option to Switch between Grid and List. Show FAQ categories as a List. Default YES ( Grid View )
+ * Option to Switch between Grid and List view in FAQs. Shows FAQ categories as a list when set to NO.
+ * Default set to YES which presents a Grid view
  */
 @property (nonatomic, assign) BOOL displayFAQsAsGrid;
 /*
- * Allow the user to attach pictures by using the camera. Default YES.
+ * Allow the user to attach images using the camera. Defaults to YES.
  */
 @property (nonatomic, assign) BOOL cameraCaptureEnabled;
 /*
- * Enable alert sound when a notification is received. Default YES.
+ * Enable alert sound when a notification is received. Defaults to YES.
  */
 @property (nonatomic, assign) BOOL notificationSoundEnabled;
 /*
@@ -50,14 +53,14 @@
  */
 @property (nonatomic, assign) BOOL agentAvatarEnabled;
 /*
- * Enable/Disable Notification banner when a support message is received. Default YES
+ * Enable/Disable Notification banner when a support message is received. Defaults to YES
  */
 @property (nonatomic, assign) BOOL showNotificationBanner;
 
 /**
  *  Initialize Hotline.
  *
- *  @discussion In order to initialize Hotline, you'll need the three parameters mentioned above. Place the Hotline initialization code in your app delegate, preferably at the top of the application:didFinishLaunchingWithOptions method.
+ *  @discussion In order to initialize Hotline, you'll need the AppID and App Key. Place the Hotline initialization code in your app delegate, preferably at the top of the application:didFinishLaunchingWithOptions method.
  *
  *  @param appKey    The App Key assigned to your app when it was created on the portal.
  *
@@ -91,43 +94,44 @@
 -(void)initWithConfig:(HotlineConfig *)config;
 
 /**
- *  Present the Conversations / Chat to the user.
+ *  Show the Conversations / Chat to the user.
  *
  *  @discussion This method lets you launch and present the Channels list to the user. The user directly lands in the Conversation view if there is only one channel.
  *
- *  @param controller This is essentially the view controller from where you're attempting to present the view.
+ *  @param controller The view controller from where you present the Conversations view.
  *
  */
 -(void)showConversations:(UIViewController *)controller;
 /**
- *  Present the FAQs to the user.
+ *  Show the FAQs to the user.
  *
- *  @discussion This method lets you launch the FAQ view.
+ *  @discussion This method lets you show the FAQ view.
  *
- *  @param controller This is essentially the view controller from where you're attempting to present the view.
+ *  @param controller The view controller from where you present the FAQ view.
  *
  */
 -(void)showFAQs:(UIViewController *)controller;
 /**
- *  Clear User Data
- *
- *  @discussion Use this function when your user needs to log out of the app . This will clean up all the data associated with the SDK for the user.
- *
- */
--(void)clearUserData;
-/**
  *  Update user Info
  *
- *  @discussion Allows you to update the user info such as Name, Email, Phone, Country Code and any Identifier. Identifier provided with the user needs to be an unique value that your App can use to identify the user.
+ *  @discussion Sends user information updates to the server. User properties such as Name, Email, Phone, Country Code and external Identifier.That are set will be synced with the server. External Identifier provided could be any unique value that your App can use to identify the user.
  *
  *  @param user User instance with the values to be updated.
  *
  */
 -(void)updateUser:(HotlineUser *) user;
 /**
+ *  Clear User Data
+ *
+ *  @discussion Use this function when your user needs to log out of the app . 
+ *  This will clean up all the data associated with the SDK for the user.
+ *
+ */
+-(void)clearUserData;
+/**
  *  Update User properties
  *
- *  @discussion Allows you tag your users with some Key Value pairs . The user properties associated here will be available on the Dashboard for the Agent.
+ *  @discussion Tag users with custom properties (key-value pairs) . The user properties associated here will be shown on the dashboard for the agent and also be used for segmentation for campaigns
  *
  *  @param props An NSDictionary containing the Properties for the User.
  *
@@ -136,7 +140,7 @@
 /**
  *  Update user property
  *
- *  @discussion Use this method to update a single property for the user. Prefer updateUserProperties whereever possible.
+ *  @discussion Use this method to update a single property for the user. Use updateUserProperties instead where possible.
  *
  *  @param key Property name
  *
@@ -154,34 +158,37 @@
  */
 -(void)updateDeviceToken:(NSData *) deviceToken;
 /**
- *  Check if a Push Notification was from Hotline
+ *  Check if a push notification was from Hotline
  *
- *  @discussion Checks if the dictionary received on a Push Notification originated from hotline. Use this in conjunction with handleRemoteNotification
+ *  @discussion Checks if the push notification received originated from Hotline by examining the payload dictionary. Use this in conjunction with handleRemoteNotification
  *
- *  @param info Dictionary received in didReceiveRemoteNotification for Push Notification.
+ *  @param info NSDictionary object in didReceiveRemoteNotification for Push Notification.
  */
 -(BOOL)isHotlineNotification:(NSDictionary *)info;
 /**
- *  Handle the Hotline message push Notification
+ *  Handle the Hotline push notifications
  *
- *  @discussion Needs to be called when a push notification is received from Hotline. This will present the conversation or shows a drop down notification.
+ *  @discussion Needs to be called when a push notification is received from Hotline. This will present the conversation if user tapped on a push, or show a drop down notification, or update a currently active conversation screen depending on context.
  *
  *  @param info Dictionary received in didReceiveRemoteNotification for Push Notification.
  *  
- *  @param appState
+ *  @param appState UIApplicationState object that helps the app be aware of whether it was already active and in the foreground when receiving the push notification, or was opened from the background
  */
 -(void)handleRemoteNotification:(NSDictionary *)info andAppstate:(UIApplicationState)appState;
 /**
- *  Get an Embeddable controller for FAQs
+ *  Get an embeddable controller for FAQs
  *
- *  @discussion Return a controller with FAQs that can be embedded in other Controllers (e.g. in a UITabBarController )
+ *  @discussion Return a controller with FAQs View that can be embedded in other Controllers (e.g. in a UITabBarController )
  *
+ *  @return UIController for FAQs View
  */
 -(UIViewController*) getFAQsControllerForEmbed;
 /**
- *  Get an Embeddable controller for Conversations
+ *  Get an embeddable controller for Conversations
  *
- *  @discussion Return a controller with Chats that can be embedded in other Controllers (e.g. in a UITabBarController )
+ *  @discussion Return a controller with Conversation view that can be embedded in other Controllers (e.g. in a UITabBarController )
+ *
+ *  @return UIController for Conversation View
  *
  */
 -(UIViewController*) getConversationsControllerForEmbed;
@@ -189,6 +196,8 @@
  *  Get the last updated unread messages count.
  *
  *  @discussion This method returns the last updated count of conversations which require the user's attention. This may not always be up to date.
+ *  
+ *  @return unread count for messages
  */
 -(NSInteger)unreadCount;
 
