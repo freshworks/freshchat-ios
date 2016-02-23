982fff34b0d6dfccd74b06ac681aba42a7cbb27b
Aravinth Chandran <arvinthchandran@gmail.com>
2016-02-24 02:18:35 +0530

    Documentation: Add appledoc in build script

--- HotlineSDK/HotlineSDK/Hotline.h
+++ HotlineSDK/HotlineSDK/Hotline.h
@@ -66,11 +66,10 @@
 /**
  *  Initialize Hotline.
  *
- *  @discussion In order to initialize Hotline, you'll need the AppID and App Key. Place the Hotline initialization code in your app delegate, preferably at the top of the application:didFinishLaunchingWithOptions method.
+ *  @discussion In order to initialize Hotline, you'll need the App ID and App Key. Place the Hotline initialization code in your app delegate, preferably at the top of the application:didFinishLaunchingWithOptions method.
  *
- *  @param appKey    The App Key assigned to your app when it was created on the portal.
- *
- *  @param appSecret The App Secret assigned to your app when it was created on the portal.
+ *  @param appID  The App ID assigned to your app when it was created on the portal.
+ *  @param appKey The App Key assigned to your app when it was created on the portal.
  *
  */
 -(instancetype)initWithAppID:(NSString*)appID andAppKey:(NSString*)appKey;
