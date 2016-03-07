8891aa6f1eefe80ad7968310be7fe55fdc9dc2d8
Aravinth Chandran <arvinthchandran@gmail.com>
2016-03-07 19:27:14 +0530

    Increment build & update version number

--- HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
@@ -10,6 +10,6 @@
 #define HLVersionConstants_h
 
 #define HOTLINE_SDK_VERSION @"1.0.0"
-#define HOTLINE_SDK_BUILD_NUMBER @"999"
+#define HOTLINE_SDK_BUILD_NUMBER @"1000"
 
 #endif /* HLVersionConstants_h */
--- Scripts/build.sh
+++ Scripts/build.sh
@@ -14,7 +14,7 @@ $*
 HEADER
 }
 
-VERSION=1.0.0
+VERSION=1.0.1
 #BUILD_NUMBER=`date +%Y%m%d%H%M`
 
 #Clear Derived Data to have clean build
