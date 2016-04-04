0c1aaf3e4185ce8e73ef7ea5511dfb64c5d262e2
Aravinth Chandran <arvinthchandran@gmail.com>
2016-04-04 16:54:05 +0530

    Disabling notification banner causes push notification not to work
    Fixes #301

--- HotlineSDK/HotlineSDK/Hotline.m
+++ HotlineSDK/HotlineSDK/Hotline.m
@@ -345,44 +345,47 @@
 
 -(void)handleRemoteNotification:(NSDictionary *)info andAppstate:(UIApplicationState)appState{
     dispatch_async(dispatch_get_main_queue(), ^{
-        
-        BOOL canShowNotification = [[FDSecureStore sharedInstance] boolValueForKey:HOTLINE_DEFAULTS_SHOW_NOTIFICATION_BANNER];
-        if(!canShowNotification)
-        return ;
 
         [HLMessageServices downloadAllMessages:nil];
-
+        
         NSDictionary *payload = [self getPayloadFromNotificationInfo:info];
+        FDLog(@"Push Recieved :%@", payload);
         
         NSNumber *channelID = @([payload[@"kon_c_ch_id"] integerValue]);
         NSString *message = [payload valueForKeyPath:@"aps.alert"];
         HLChannel *channel = [HLChannel getWithID:channelID inContext:[KonotorDataManager sharedInstance].mainObjectContext];
+        
         if (!channel) return;
         
-        FDNotificationBanner *banner = [FDNotificationBanner sharedInstance];
-        [banner setMessage:message];
-        banner.delegate = self;
-        
-        HLChannel *visibleChannel = [HotlineAppState sharedInstance].currentVisibleChannel;
-                
-        if(visibleChannel){
-            if ([visibleChannel.channelID isEqual:channel.channelID]) {
-                FDLog(@"Do not display notification banner / handle notification");
-            }else{
-                [banner displayBannerWithChannel:channel];
-                FDLog(@"Display notification banner, user is in some other channel");
-            }
-        }else{
+        BOOL canShowBanner = [[FDSecureStore sharedInstance] boolValueForKey:HOTLINE_DEFAULTS_SHOW_NOTIFICATION_BANNER];
+        if(!canShowBanner){
             if (appState == UIApplicationStateInactive) {
                 [self launchMessageControllerOfChannel:channel];
-                FDLog(@"Take user to the appropriate message screen");
+            }
+        }else{
+            FDNotificationBanner *banner = [FDNotificationBanner sharedInstance];
+            [banner setMessage:message];
+            banner.delegate = self;
+            
+            HLChannel *visibleChannel = [HotlineAppState sharedInstance].currentVisibleChannel;
+            
+            if(visibleChannel){
+                if ([visibleChannel.channelID isEqual:channel.channelID]) {
+                    FDLog(@"Do not display notification banner / handle notification");
+                }else{
+                    [banner displayBannerWithChannel:channel];
+                    FDLog(@"Display notification banner, user is in some other channel");
+                }
             }else{
-                [banner displayBannerWithChannel:channel];
-                FDLog(@"Display notification banner, user is somewhere outside channel screen");
+                if (appState == UIApplicationStateInactive) {
+                    [self launchMessageControllerOfChannel:channel];
+                    FDLog(@"Take user to the appropriate message screen");
+                }else{
+                    [banner displayBannerWithChannel:channel];
+                    FDLog(@"Display notification banner, user is somewhere outside channel screen");
+                }
             }
         }
-        
-        FDLog(@"Push Recieved :%@", payload);
     });
 }
 
