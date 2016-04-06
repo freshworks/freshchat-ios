4f7ad75c728f545c07b94f9c6df52e7e78cf6b92
Aravinth Chandran <arvinthchandran@gmail.com>
2016-04-06 15:53:07 +0530

    Fix multiple notif background state issue

--- HotlineSDK/HotlineSDK/HLNotificationHandler.m
+++ HotlineSDK/HotlineSDK/HLNotificationHandler.m
@@ -42,7 +42,6 @@
 }
 
 - (void) handleNotificationBanner :(HLChannel *)channel withMessage:(NSString *)message andState:(UIApplicationState)state{
-    
     if (state == UIApplicationStateInactive) {
         [self launchMessageControllerOfChannel:channel];
     }
@@ -58,13 +57,14 @@
 -(void)launchMessageControllerOfChannel:(HLChannel *)channel{
     UIViewController *visibleSDKController = [HotlineAppState sharedInstance].currentVisibleController;
     if (visibleSDKController) {
-        FDLog(@"visible screen is inside SDK");
         if ([visibleSDKController isKindOfClass:[HLChannelViewController class]]) {
             [self pushMessageControllerFrom:visibleSDKController.navigationController withChannel:channel];
         } else if ([visibleSDKController isKindOfClass:[FDMessageController class]]) {
             FDMessageController *msgController = (FDMessageController *)visibleSDKController;
             if (msgController.isModal) {
-                [self presentMessageControllerOn:visibleSDKController withChannel:channel];
+                if (![channel isActiveChannel]) {
+                    [self presentMessageControllerOn:visibleSDKController withChannel:channel];
+                }
             }else{
                 UINavigationController *navController = msgController.navigationController;
                 [navController popViewControllerAnimated:NO];
