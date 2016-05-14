e760eb1424e8900355da14ec7db8c34bea6ba230
Hrishikesh Premkumar <hrishikesh@freshdesk.com>
2016-05-14 14:50:12 +0530

    Unread count dispatch on main thread
    Fixes #367

--- HotlineSDK/HotlineSDK/Hotline.m
+++ HotlineSDK/HotlineSDK/Hotline.m
@@ -442,7 +442,11 @@
 
 -(void)unreadCountWithCompletion:(void (^)(NSInteger count))completion{
     [HLMessageServices fetchChannelsAndMessages:^(NSError *error) {
-        if (completion) completion([self unreadCount]);
+        if (completion) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                 completion([self unreadCount]);
+            });
+        }
     }];
 }
 
