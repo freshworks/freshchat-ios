2acd5f5644fa8e8370e5ce81b2519673bb142151
Hrishikesh Premkumar <hrishikesh@freshdesk.com>
2016-06-24 20:15:19 +0530

    Remove feature check

--- HotlineSDK/HotlineSDK/View/FDCell.m
+++ HotlineSDK/HotlineSDK/View/FDCell.m
@@ -201,12 +201,8 @@ static float height = 0;
     label.layer.cornerRadius = label.frame.size.height / 2.0f;
     label.clipsToBounds = YES;
     
-    if (&UIGraphicsBeginImageContextWithOptions != NULL) {
-        UIGraphicsBeginImageContextWithOptions(label.frame.size, NO, 0.0);
-    } else {
-        UIGraphicsBeginImageContext(label.frame.size);
-    }
-
+    UIGraphicsBeginImageContextWithOptions(label.frame.size, NO, 0.0);
+    
     [[label layer] renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
