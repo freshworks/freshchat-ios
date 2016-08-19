ec37af18e74842a26f4d8f0c7229d65ac1452dae
Hrishikesh Premkumar <hrishikesh@freshdesk.com>
2016-08-19 20:04:55 +0530

    Release [1.1.3] Build[1019] - Hrishikesh Premkumar

--- HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef HLVersionConstants_h
 #define HLVersionConstants_h
 
-#define HOTLINE_SDK_VERSION @"1.1.2"
-#define HOTLINE_SDK_BUILD_NUMBER @"1018"
+#define HOTLINE_SDK_VERSION @"1.1.3"
+#define HOTLINE_SDK_BUILD_NUMBER @"1019"
 
 #endif /* HLVersionConstants_h */
