3aca30b101fa5cbc065a8b55f4b7161477f6d373
AravinthChandran <arvinthchandran@gmail.com>
2016-10-31 17:14:43 +0530

    Limit API logs

--- HotlineSDK/HotlineSDK/Network/HLAPIClient.m
+++ HotlineSDK/HotlineSDK/Network/HLAPIClient.m
@@ -9,6 +9,8 @@
 #import "HLAPIClient.h"
 #import "FDMemLogger.h"
 
+static NSMutableArray *LOGGED_API_CALLS;
+
 @interface HLAPIClient ()
 
 @property(nonatomic, strong) NSURLSession *session;
@@ -18,6 +20,11 @@
 @implementation HLAPIClient
 
 +(id)sharedInstance{
+
+    if (LOGGED_API_CALLS == nil) {
+        LOGGED_API_CALLS = [[NSMutableArray alloc]init];
+    }
+    
     static HLAPIClient *sharedInstance = nil;
     static dispatch_once_t oncetoken;
     dispatch_once(&oncetoken,^{
@@ -41,10 +48,14 @@
         FDResponseInfo *responseInfo = [[FDResponseInfo alloc]initWithResponse:response andHTTPBody:data];
         if (statusCode >= 400) {
             
-            FDMemLogger *logger = [FDMemLogger new];
-            [logger addErrorInfo:@{ @"request" : request.toString, @"response" : responseInfo.toString}];
-            [logger upload];
-            
+
+            if (![LOGGED_API_CALLS containsObject:request.URL.absoluteString]) {
+                [LOGGED_API_CALLS addObject:request.URL.absoluteString];
+                FDMemLogger *logger = [FDMemLogger new];
+                [logger addErrorInfo:@{ @"request" : request.toString, @"response" : responseInfo.toString}];
+                [logger upload];
+            }
+        
             NSDictionary *info = @{ @"Status code" : [NSString stringWithFormat:@"%ld", (long)statusCode] };
             if (handler) handler(responseInfo,[NSError errorWithDomain:@"Request failed" code:statusCode userInfo:info]);
         }else{
