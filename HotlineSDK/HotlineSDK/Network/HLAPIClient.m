6e9a330a50050546dfdff91ac0dec3fc7bf40502
AravinthChandran <arvinthchandran@gmail.com>
2016-10-31 19:48:27 +0530

    Add review changes

--- HotlineSDK/HotlineSDK/Network/HLAPIClient.m
+++ HotlineSDK/HotlineSDK/Network/HLAPIClient.m
@@ -9,22 +9,23 @@
 #import "HLAPIClient.h"
 #import "FDMemLogger.h"
 
-static NSMutableArray *LOGGED_API_CALLS;
-
 @interface HLAPIClient ()
 
+@property(nonatomic, strong) NSMutableArray *loggedAPICalls;
 @property(nonatomic, strong) NSURLSession *session;
 
 @end
 
 @implementation HLAPIClient
 
-+(id)sharedInstance{
-
-    if (LOGGED_API_CALLS == nil) {
-        LOGGED_API_CALLS = [[NSMutableArray alloc]init];
+-(NSMutableArray *)loggedAPICalls{
+    if (!_loggedAPICalls) {
+        _loggedAPICalls = [[NSMutableArray alloc]init];
     }
-    
+    return _loggedAPICalls;
+}
+
++(id)sharedInstance{
     static HLAPIClient *sharedInstance = nil;
     static dispatch_once_t oncetoken;
     dispatch_once(&oncetoken,^{
@@ -48,9 +49,8 @@ static NSMutableArray *LOGGED_API_CALLS;
         FDResponseInfo *responseInfo = [[FDResponseInfo alloc]initWithResponse:response andHTTPBody:data];
         if (statusCode >= 400) {
             
-
-            if (![LOGGED_API_CALLS containsObject:request.URL.absoluteString]) {
-                [LOGGED_API_CALLS addObject:request.URL.absoluteString];
+            if (![self.loggedAPICalls containsObject:request.URL.path]) {
+                [self.loggedAPICalls addObject:request.URL.path];
                 FDMemLogger *logger = [FDMemLogger new];
                 [logger addErrorInfo:@{ @"request" : request.toString, @"response" : responseInfo.toString}];
                 [logger upload];
