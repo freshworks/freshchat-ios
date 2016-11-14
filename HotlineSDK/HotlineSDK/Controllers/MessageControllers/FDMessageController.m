a00cbd8043b329fefe33f1e12f3abe7fa6f9fb57
harish6989 <harishk@freshdesk.com>
2016-11-14 13:15:46 +0530

    unsubscribe audio notification

--- HotlineSDK/HotlineSDK/Controllers/MessageControllers/FDMessageController.m
+++ HotlineSDK/HotlineSDK/Controllers/MessageControllers/FDMessageController.m
@@ -542,6 +542,8 @@ typedef struct {
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"HLPlayAppAudioNotification" object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"HLPauseAppAudioNotification" object:nil];
 }
 
 -(void)handleBecameActive:(NSNotification *)notification{
