b461d00261c8afadb5f72b4b17f0d7b57db609e8
AravinthChandran <arvinthchandran@gmail.com>
2016-12-01 08:03:54 +0530

    Fix missed prefix renames

--- HotlineSDK/HotlineSDK/Model/KonotorDataManager.h
+++ HotlineSDK/HotlineSDK/Model/KonotorDataManager.h
@@ -11,7 +11,7 @@
 #import <CoreData/CoreData.h>
 
 #define HOTLINE_ARTICLE_ENTITY @"HLArticle"
-#define HOTLINE_TAGS_ENTITY @"FDTags"
+#define HOTLINE_TAGS_ENTITY @"HLTags"
 #define HOTLINE_CATEGORY_ENTITY @"HLCategory"
 #define HOTLINE_CHANNEL_ENTITY @"HLChannel"
 #define HOTLINE_INDEX_ENTITY @"FDIndex"
