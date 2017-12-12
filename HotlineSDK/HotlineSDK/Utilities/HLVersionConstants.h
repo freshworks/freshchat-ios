c7bcc50a50b98a35df90c8389d59a35cb5f32a5d
Sanjith Jothi Kanagavel <sanjith@freshdesk.com>
2017-12-12 14:26:17 +0530

    Updating the missed release notes. (#72)

--- HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef HLVersionConstants_h
 #define HLVersionConstants_h
 
-#define HOTLINE_SDK_VERSION @"1.2.1"
-#define HOTLINE_SDK_BUILD_NUMBER @"21"
+#define HOTLINE_SDK_VERSION @"1.2.2"
+#define HOTLINE_SDK_BUILD_NUMBER @"22"
 
 #endif /* HLVersionConstants_h */
