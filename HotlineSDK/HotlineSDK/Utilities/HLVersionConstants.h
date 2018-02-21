d8bc389ef71979135912ca6ae0c0ec1466647b07
Harish Kumar <harishk@freshdesk.com>
2018-02-21 19:15:29 +0530

    Release [1.3.1] Build[31] - Harish Kumar

--- HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/HLVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef HLVersionConstants_h
 #define HLVersionConstants_h
 
-#define HOTLINE_SDK_VERSION @"1.3.0"
-#define HOTLINE_SDK_BUILD_NUMBER @"30"
+#define HOTLINE_SDK_VERSION @"1.3.1"
+#define HOTLINE_SDK_BUILD_NUMBER @"31"
 
 #endif /* HLVersionConstants_h */
