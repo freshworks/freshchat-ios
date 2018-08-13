54326aa0a6aff813c45ddd85ef70d710a130e618
Harish Kumar <harishk@freshdesk.com>
2018-08-13 18:11:36 +0530

    Reset build and sdk version

--- HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef FCVersionConstants_h
 #define FCVersionConstants_h
 
-#define FRESHCHAT_SDK_VERSION @"1.5.0"
-#define FRESHCHAT_SDK_BUILD_NUMBER @"50"
+#define FRESHCHAT_SDK_VERSION @"1.4.9"
+#define FRESHCHAT_SDK_BUILD_NUMBER @"49"
 
 #endif /* FCVersionConstants_h */
