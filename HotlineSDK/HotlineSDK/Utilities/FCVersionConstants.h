ce2ddae5201323046cdcedbf64fce7cb92a0d57f
Harish Kumar <harishk@freshdesk.com>
2018-10-12 19:32:07 +0530

    Release [1.5.5] Build[55] - Harish Kumar

--- HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef FCVersionConstants_h
 #define FCVersionConstants_h
 
-#define FRESHCHAT_SDK_VERSION @"1.5.4"
-#define FRESHCHAT_SDK_BUILD_NUMBER @"54"
+#define FRESHCHAT_SDK_VERSION @"1.5.5"
+#define FRESHCHAT_SDK_BUILD_NUMBER @"55"
 
 #endif /* FCVersionConstants_h */
