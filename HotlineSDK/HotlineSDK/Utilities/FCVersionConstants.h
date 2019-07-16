97221a13c2997c426de6fba608433e5920488746
Harish Kumar <harishk@freshdesk.com>
2019-07-16 13:41:09 +0530

    Release [2.6.0] Build[260] - Harish Kumar

--- HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef FCVersionConstants_h
 #define FCVersionConstants_h
 
-#define FRESHCHAT_SDK_VERSION @"2.5.9"
-#define FRESHCHAT_SDK_BUILD_NUMBER @"259"
+#define FRESHCHAT_SDK_VERSION @"2.6.0"
+#define FRESHCHAT_SDK_BUILD_NUMBER @"260"
 
 #endif /* FCVersionConstants_h */
