0e84681594cde1fd694947e522583626e38ee3d2
Harish <harish.kumar@freshworks.com>
2019-08-29 13:04:31 +0530

    Swift 4 changes (#221)

--- Hotline/L102Language.swift
+++ Hotline/L102Language.swift
@@ -13,7 +13,7 @@ let APPLE_LANGUAGE_KEY = "AppleLanguages"
 /// L102Language
 class L102Language: NSObject {
     /// get current Apple language
-    class func currentAppleLanguage() -> String{
+    @objc class func currentAppleLanguage() -> String{
         let userdef = UserDefaults.standard
         let langArray = userdef.object(forKey: APPLE_LANGUAGE_KEY) as! NSArray
         let current = langArray.firstObject as! String
@@ -22,7 +22,7 @@ class L102Language: NSObject {
         return currentWithoutLocale
     }
     
-    class func currentAppleLanguageFull() -> String{
+    @objc class func currentAppleLanguageFull() -> String{
         let userdef = UserDefaults.standard
         let langArray = userdef.object(forKey: APPLE_LANGUAGE_KEY) as! NSArray
         let current = langArray.firstObject as! String
@@ -30,13 +30,13 @@ class L102Language: NSObject {
     }
     
     /// set @lang to be the first in Applelanguages list
-    class func setAppleLAnguageTo(lang: String) {
+    @objc class func setAppleLAnguageTo(lang: String) {
         let userdef = UserDefaults.standard
         userdef.set([lang,currentAppleLanguage()], forKey: APPLE_LANGUAGE_KEY)
         userdef.synchronize()
     }
 
-    class var isRTL: Bool {
+    @objc class var isRTL: Bool {
         return L102Language.currentAppleLanguage() == "ar"
     }
 }
