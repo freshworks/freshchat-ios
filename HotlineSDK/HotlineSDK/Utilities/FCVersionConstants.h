56fad1eaf6760368b5170369dfc30b2c0b9a9e74
Harish kumar <harish.kumar@freshworks.com>
2020-02-18 18:22:05 +0530

    Release [3.2.2] Build[322] -

--- HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
+++ HotlineSDK/HotlineSDK/Utilities/FCVersionConstants.h
@@ -9,7 +9,7 @@
 #ifndef FCVersionConstants_h
 #define FCVersionConstants_h
 
-#define FRESHCHAT_SDK_VERSION @"3.2.1"
-#define FRESHCHAT_SDK_BUILD_NUMBER @"321"
+#define FRESHCHAT_SDK_VERSION @"3.2.2"
+#define FRESHCHAT_SDK_BUILD_NUMBER @"322"
 
 #endif /* FCVersionConstants_h */
