9a5a4d0a851753e0a6cb5000198a33c6f50b278c
Hemanth Kumar <hemanth.kumar@freshworks.com>
2021-03-09 13:44:30 +0530

    Added support to SPM and XCframework

---
+++ FreshchatSDK.xcframework/ios-arm64_x86_64-simulator/FreshchatSDKScheme.framework/Headers/Freshchat.h
@@ -0,0 +1,9 @@
+//
+//  Freshchat.h
+//  HotlineSDK
+//
+//  Created by Hemanth Kumar on 13/02/21.
+//  Copyright © 2021 Freshdesk. All rights reserved.
+//
+
+#import <FreshchatSDK/FreshchatSDK.h>
---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "FreshchatSDK",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "FreshchatSDK",
+            targets: [
+              "FreshchatSDKTarget"
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "FreshchatSDKTarget",
+            path: "FreshchatSDK.xcframework"
+        )
+    ]
+)
