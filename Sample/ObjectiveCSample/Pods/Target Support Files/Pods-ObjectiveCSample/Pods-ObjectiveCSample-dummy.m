a1275b12be2fe202cab79a37f64bd156d5a89fe6
saravanan-periyasamy <saravanan.periyasamy@freshworks.com>
2025-01-23 11:29:04 +0530

    Update SDK to v6.3.3 (#164)
    * Update SDK to v6.3.3
    
    * Empty package added to ensure SPM installation excluded sample apps
    
    * - Podfile.lock and Pods folder removed from sample apps
    - gitignore updated to exclude Pods and pofile.lock
    
    * - Old Swift and Objective-C samples removed
    - New Swift sample added
    - Podfiles and Pods removed - dependency installed with SPN in sample app
    
    * - Info plist and launch screen updated in new sample app
    
    * Package file removed
    
    * Package.swift file re-added
    
    * Package file re-placed
    
    * swiftpm data updated
    
    * Replaced Package.swift file
    
    * Automatic signing disabled
    
    ---------
    
    Co-authored-by: pramit-tewari <pramit.tewari@freshworks.com>

--- Sample/ObjectiveCSample/Pods/Target Support Files/Pods-ObjectiveCSample/Pods-ObjectiveCSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ObjectiveCSample : NSObject
-@end
-@implementation PodsDummy_Pods_ObjectiveCSample
-@end
--- Sample/SwiftSample/SwiftSample/ViewController.swift
+++ Sample/SwiftSample/SwiftSample/ViewController.swift
@@ -2,31 +2,25 @@
 //  ViewController.swift
 //  SwiftSample
 //
-//  Created by user on 18/09/17.
-//  Copyright © 2017 Sanjith J K. All rights reserved.
+//  Created by Pramit Tewari on 17/01/25.
 //
 
 import UIKit
+import FreshchatSDK
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func showFAQs(_ sender: Any) {
-        Freshchat.sharedInstance().showFAQs(self)
-    }
-    
-    @IBAction func showConversations(_ sender: Any) {
+    @IBAction func showConversations() {
         Freshchat.sharedInstance().showConversations(self)
     }
-
+    
+    @IBAction func showFAQs() {
+        Freshchat.sharedInstance().showFAQs(self)
+    }
 }
 
